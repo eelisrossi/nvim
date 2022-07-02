c2679a588988fec2da977beb2fed575cff13ec91
Eelis Rossi <eelis@zarguon.local>
2022-07-02 12:23:21 +0300

    Added autocomplete

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,7 +44,9 @@ return packer.startup(function(use)
     use "wbthomason/packer.nvim"            -- Have packer manage itself
     use "nvim-lua/popup.nvim"               -- An implementation of the Popup API from vim in Neovim
     use "nvim-lua/plenary.nvim"             -- Useful lua functions used by lots of plugins
+    use "windwp/nvim-autopairs"             -- Autopairs stuff
 
+    -- cmp plugins
     use "hrsh7th/nvim-cmp"                  -- The completion plugin
     use "hrsh7th/cmp-buffer"                -- buffer completions
     use "hrsh7th/cmp-path"                  -- path completions
@@ -63,6 +65,8 @@ return packer.startup(function(use)
 
     use "drewtempelmeyer/palenight.vim"
 
+    use "sbdchd/neoformat"
+    use "dense-analysis/ale"
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
