da253174ad6e7ea3eab757e57c4e187d4c39be3e
Eelis Rossi <eelis.rossi@gmail.com>
2023-12-14 15:13:23 +0200

    added tmux-friendly binds for harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -8,6 +8,11 @@ vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end, { desc 
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-k>", function() harpoon:list():select(7) end)
+vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<C-ö>", function() harpoon:list():select(4) end)
+
+vim.keymap.set("n", "<leader>j", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>k", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>l", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>ö", function() harpoon:list():select(4) end)
