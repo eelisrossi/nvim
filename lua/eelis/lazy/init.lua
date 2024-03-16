e66014921aaf80edf5501078d53ff1d772bb2d21
Eelis Rossi <eelis.rossi@gmail.com>
2024-03-16 12:54:42 +0200

    copied some stuff primeagens new nvimrc, minor modifications to keymaps

--- lua/eelis/lazy/init.lua
+++ lua/eelis/lazy/init.lua
@@ -3,47 +3,47 @@ return {
   -- -- ui/theme-related stuff
   -- { "catppuccin/nvim",        name = "catppuccin", priority = 1000 },
 
-  -- autocompletion & lsp
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
-
-      -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
-    },
-    config = function()
-      require("eelis.config.nvim-cmp")
-    end,
-  },
-
-  {
-    'williamboman/mason.nvim',
-    config = function()
-      require("eelis.config.mason")
-    end,
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufRead", "BufNewFile" },
-    dependencies = {
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-
-      { "j-hui/fidget.nvim", opts = {} },
-
-      'folke/neodev.nvim',
-    },
-    config = function()
-      require("eelis.config.lsp")
-    end,
-  },
+  -- -- autocompletion & lsp
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   dependencies = {
+  --     -- Snippet Engine & its associated nvim-cmp source
+  --     'L3MON4D3/LuaSnip',
+  --     'saadparwaiz1/cmp_luasnip',
+  --
+  --     -- Adds LSP completion capabilities
+  --     'hrsh7th/cmp-nvim-lsp',
+  --
+  --     -- Adds a number of user-friendly snippets
+  --     'rafamadriz/friendly-snippets',
+  --   },
+  --   config = function()
+  --     require("eelis.config.nvim-cmp")
+  --   end,
+  -- },
+  --
+  -- {
+  --   'williamboman/mason.nvim',
+  --   config = function()
+  --     require("eelis.config.mason")
+  --   end,
+  -- },
+  --
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   event = { "BufRead", "BufNewFile" },
+  --   dependencies = {
+  --     'williamboman/mason.nvim',
+  --     'williamboman/mason-lspconfig.nvim',
+  --
+  --     { "j-hui/fidget.nvim", opts = {} },
+  --
+  --     'folke/neodev.nvim',
+  --   },
+  --   config = function()
+  --     require("eelis.config.lsp")
+  --   end,
+  -- },
 
   -- git related things
   { "lewis6991/gitsigns.nvim" },
