21ba2bda9f4caf3f99679de0069ebbdfbd1e7649
Eelis Rossi <eelis.rossi@gmail.com>
2024-10-08 08:20:09 +0300

    cleaning some stuff up

--- lua/eelis/config/mason.lua
+++ lua/eelis/config/mason.lua
@@ -1,24 +1,7 @@
--- mason-lspconfig requires that these setup functions are called in this order
--- before setting up the servers.
 require('mason').setup()
 require('mason-lspconfig').setup()
 
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
-
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
--- lua/eelis/lazy/init.lua
+++ lua/eelis/lazy/init.lua
@@ -1,74 +1,39 @@
 return {
 
-  -- -- ui/theme-related stuff
-  -- { "catppuccin/nvim",        name = "catppuccin", priority = 1000 },
 
-  -- -- autocompletion & lsp
-  -- {
-  --   "hrsh7th/nvim-cmp",
-  --   dependencies = {
-  --     -- Snippet Engine & its associated nvim-cmp source
-  --     'L3MON4D3/LuaSnip',
-  --     'saadparwaiz1/cmp_luasnip',
-  --
-  --     -- Adds LSP completion capabilities
-  --     'hrsh7th/cmp-nvim-lsp',
-  --
-  --     -- Adds a number of user-friendly snippets
-  --     'rafamadriz/friendly-snippets',
-  --   },
-  --   config = function()
-  --     require("eelis.config.nvim-cmp")
-  --   end,
-  -- },
-  --
-  -- {
-  --   'williamboman/mason.nvim',
-  --   config = function()
-  --     require("eelis.config.mason")
-  --   end,
-  -- },
-  --
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   event = { "BufRead", "BufNewFile" },
-  --   dependencies = {
-  --     'williamboman/mason.nvim',
-  --     'williamboman/mason-lspconfig.nvim',
-  --
-  --     { "j-hui/fidget.nvim", opts = {} },
-  --
-  --     'folke/neodev.nvim',
-  --   },
-  --   config = function()
-  --     require("eelis.config.lsp")
-  --   end,
-  -- },
-
-  -- git related things
-  { "lewis6991/gitsigns.nvim" },
-  { 'tpope/vim-fugitive' },
-  { 'tpope/vim-rhubarb' },
+    -- git related things
+    { "lewis6991/gitsigns.nvim" },
+    { 'tpope/vim-fugitive' },
+    { 'tpope/vim-rhubarb' },
 
 
-  -- utility stuff
+    -- utility stuff
 
-  { 'folke/which-key.nvim',  opts = {} },
+    { 'folke/which-key.nvim',   opts = {} },
 
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("eelis.config.statusline")
-    end,
-  },
+    {
+        "nvim-lualine/lualine.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("eelis.config.statusline")
+        end,
+    },
 
-  { 'numToStr/Comment.nvim', opts = {} },
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    opts = {}
-  },
+    { 'numToStr/Comment.nvim', opts = {} },
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        opts = {}
+    },
+
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            highlight = {
+                comments_only = false,
+            }
+        }
+    }
 
 }
-
